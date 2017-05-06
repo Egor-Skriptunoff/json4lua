ce92f0a08f8e56d37a8d952d93143289fd141d47
Egor_Skriptunoff <egor.skriptunoff@gmail.com>
2017-05-06 06:14:56 +0300

    Some improvements

--- README.md
+++ README.md
@@ -39,3 +39,102 @@ Primes are:
         3       5
         4       7
 ```
+
+### Traversing (decoding JSON without creating the result on Lua side)
+
+function  `json.traverse(s, callback, pos)`  traverses JSON and invokes user-supplied callback function
+```
+   callback function arguments: (path, json_type, value, pos)
+      path      is array of nested JSON identifiers, this array is empty for root JSON element
+      json_type is one of "null"/"boolean"/"number"/"string"/"array"/"object"
+      value     is defined when json_type is "boolean"/"number"/"string", otherwise value == nil
+      pos       is 1-based index of first character of current JSON element inside JSON string
+```
+```lua
+json.traverse([[ 42 ]], callback)
+-- will invoke callback function 1 time:
+   callback( {},         "number",  42,    2  )
+```
+```lua
+json.traverse([[ {"a":true, "b":null, "c":["one","two"], "d":{ "e":{}, "f":[] } } ]], callback)
+-- will invoke callback function 9 times:
+   callback( {},         "object",  nil,   2  )
+   callback( {"a"},      "boolean", true,  7  )
+   callback( {"b"},      "null",    nil,   17 )
+   callback( {"c"},      "array",   nil,   27 )
+   callback( {"c", 1},   "string",  "one", 28 )
+   callback( {"c", 2},   "string",  "two", 34 )
+   callback( {"d"},      "object",  nil,   46 )
+   callback( {"d", "e"}, "object",  nil,   52 )
+   callback( {"d", "f"}, "array",   nil,   60 )
+```
+
+
+### Reading JSON chunk-by-chunk
+
+Both decoder functions `json.decode()` and `json.traverse()` can accept JSON (argument s) as a "loader function" instead of a string.
+This function will be called repeatedly to return next parts (substrings) of JSON string.
+An empty string, nil, or no value returned from "loader function" means the end of JSON string.
+This may be useful for low-memory devices or for traversing huge JSON files.
+
+
+### Partial decoding of arbitrary element inside JSON
+
+Instead of decoding whole JSON, you can decode its arbitrary element (e.g, array or object) by specifying the position where this element starts.
+In order to do that, at first you have to traverse JSON to get all positions you need.
+
+
+### Partial decoding of JSON with reading JSON from file
+
+```lua
+-- This is content of "data.txt" file:
+-- {"aa":["qq",{"k1":23,"gg":"YAY","Fermat_primes":[3, 5, 17, 257, 65537],"bb":0}]}
+-- We want to extract only Fermat primes from this JSON (and save it as Lua array) without loading whole JSON to Lua
+
+local json = require('json')
+
+-- Open file
+local file = io.open('data.txt', 'r')
+
+-- Define loader function which will read the file in 64-byte chunks
+local function my_json_loader()
+   return file:read(64)
+end
+
+-- Prepare callback function for traverse
+local position  -- We need to know the position of Fermat primes array inside this JSON
+local function my_callback (path, json_type, value, pos)
+   if #path == 3
+      and path[1] == "aa"
+      and path[2] == 2
+      and path[3] == "Fermat_primes"
+      and json_type == "array"
+   then
+      position = pos
+   end
+end
+
+-- Step #1: Traverse to get the position
+file:seek("set", 0)
+json.traverse(my_json_loader, my_callback)
+
+-- Step #2: Decode only Fermat_primes array
+file:seek("set", position - 1)
+local FP = json.decode(my_json_loader)
+print('Fermat_primes:')
+for k, v in ipairs(FP) do print(k, v) end
+
+-- Close file
+file:close()
+```
+
+Output:
+
+```
+Fermat_primes:
+1  3
+2  5
+3  17
+4  257
+5  65537
+```
--- example.lua
+++ example.lua
@@ -57,3 +57,147 @@ print()
 local someJSON = [[ "Test \u00A7 \"\ud834\udd1e\" \\\uD83D\uDE02\/ " ]]
 print('JSON: <'..someJSON..'>')
 print('Decoded: <'..json.decode(someJSON)..'>')  -->  Test § "𝄞" \😂/
+
+------------------------
+-- decoding a JSON array with omitted values
+------------------------
+print()
+local test_string = [[ [,,42,,] ]]
+local result = json.decode(test_string)
+print ("The decoded table result:")
+for k, v in pairs(result) do print('', k, v) end
+
+------------------------
+-- decoding a JSON object with omitted values
+------------------------
+print()
+local test_string = [[ {,,a:true,,,a$b_5:-0.01e99,,} ]]
+local result = json.decode(test_string)
+print ("The decoded table result:")
+for k, v in pairs(result) do print('', type(k), k, type(v), v) end
+
+------------------------
+-- show information passed to callback function during JSON traverse
+------------------------
+local function callback(path, json_type, value, pos)
+   print('callback', table.concat(path, '/'), json_type, value, pos)
+end
+
+print()
+print('--------', 'path', 'j_type', 'value', 'pos')
+json.traverse([[ 42 ]], callback)
+
+print()
+print('--------', 'path', 'j_type', 'value', 'pos')
+json.traverse([[ {"a":true, "b":null, "c":["one","two"], "d":{ "e":{}, "f":[] } } ]], callback)
+
+------------------------
+-- traversing JSON string to search for data
+------------------------
+print()
+local test_string = [[ { "the_answer":42, "greeting":"Hello", "Fermat_primes":[3, 5, 17, 257, 65537], "math_consts":{ "e":2.72, "pi":3.14 } } ]]
+
+-- All we need to know from this JSON is: all Fermat primes, math constant "e" and greeting word
+local e, greeting
+local function my_callback (path, json_type, value)
+   if
+      -- print all numeric items of "Fermat_primes"
+      #path == 2
+      and path[1] == "Fermat_primes"
+      and json_type == "number"
+   then
+      print("F"..path[2].." = "..value)  -- index (1-based) and value
+   elseif
+      -- full check: whole path array and json_type are checked to be what we want
+      #path == 1
+      and path[1] == "greeting"
+      and json_type == "string"
+   then
+      greeting = value
+   elseif
+      -- simplified check: we assume here that field with name "e" is unique inside this JSON
+      path[#path] == "e"
+   then
+      e = value
+   end
+end
+
+-- Do traversing (and print Fermat primes on-the-fly)
+json.traverse(test_string, my_callback)
+
+-- Show what was saved during the traverse
+print(e, greeting)
+
+
+------------------------
+-- partial decoding of JSON
+------------------------
+-- If you want to decode only some element (i.e, array) inside JSON instead of decoding of whole JSON,
+-- you can at first traverse JSON to get position of your element
+-- and then decode this element by specifying its position
+
+print()
+local test_string = [[ { "the_answer":42, "greeting":"Hello", "Fermat_primes":[3, 5, 17, 257, 65537], "math_consts":{ "e":2.72, "pi":3.14 } } ]]
+
+-- We need to know the position of Fermat primes inside this JSON
+local position
+local function my_callback (path, json_type, value, pos)
+   if #path == 1
+      and path[1] == "Fermat_primes"
+      and json_type == "array"
+   then
+      position = pos
+   end
+end
+-- Do traversing to get the position
+json.traverse(test_string, my_callback)
+-- Show the position of Fermat_primes inside JSON string
+print(position)
+-- Do partial decoding to get Fermat_primes
+local result = json.decode(test_string, position)
+print ("The Fermat_primes array (as result of partial decoding):")
+for k, v in ipairs(result) do print('', k, v) end
+
+
+------------------------
+-- partial decoding of JSON with reading JSON from file
+------------------------
+-- This is content of "data.txt" file:
+-- {"aa":["qq",{"k1":23,"gg":"YAY","Fermat_primes":[3, 5, 17, 257, 65537],"bb":0}]}
+-- We want to extract only Fermat primes from this JSON (and save it as Lua array)
+
+print()
+
+-- Open file
+local file = io.open('data.txt', 'r')
+
+-- Define loader function which will read the file in 64-byte chunks
+local function my_json_loader()
+   return file:read(64)
+end
+
+-- Prepare callback function for traverse
+local position  -- We need to know the position of Fermat primes inside this JSON
+local function my_callback (path, json_type, value, pos)
+   if #path == 3
+      and path[1] == "aa"
+      and path[2] == 2
+      and path[3] == "Fermat_primes"
+      and json_type == "array"
+   then -- remember position of this array to decode it on step #2
+      position = pos
+   end
+end
+
+-- Step #1: Traverse to get the position
+file:seek("set", 0)
+json.traverse(my_json_loader, my_callback)
+
+-- Step #2: Decode only Fermat_primes
+file:seek("set", position - 1)
+local FP = json.decode(my_json_loader)
+print('Fermat_primes:')
+for k, v in ipairs(FP) do print(k, v) end
+
+-- Close file
+file:close()
